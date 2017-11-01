be097d26732eed68a13a2a5b5bda597532fda7f5
Dun Ma <madun@hotmail.com>
2017-11-01 08:45:43 -0500

    Prepare for selecting Avatar

--- LookingFor/app/src/main/java/vivekvaidya/com/lookingfor/User.java
+++ LookingFor/app/src/main/java/vivekvaidya/com/lookingfor/User.java
@@ -4,7 +4,15 @@ package vivekvaidya.com.lookingfor;
  * Created by Administrator on 2017/10/26.
  */
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class User implements Serializable {
@@ -12,7 +20,6 @@ public class User implements Serializable {
     final private String UID;
     private String emailAddress;
     private String phoneNumber;
-    private String passWord;
     private String userName;
     private String avatarLink;
     private boolean isLoggedIn = false;
@@ -49,14 +56,6 @@ public class User implements Serializable {
             this.emailAddress = emailAddress;
         }
     }
-    public String getPassWord(){
-        return this.passWord;
-    }
-    public void setPassWord(String passWord){
-        if (this.isLoggedIn){
-            this.passWord = passWord;
-        }
-    }
     public String getUserName(){
         return this.userName;
     }
@@ -78,6 +77,29 @@ public class User implements Serializable {
     }
     public void swapLogin(){
         this.isLoggedIn = !this.isLoggedIn;
+    }
+
+    /***/
+    public void pushToFirebase(OnCompleteListener<Void> onCompleteListener){
+        HashMap<String, Object> dataMap = new HashMap<>();
+        dataMap.put("email", getEmailAddress());
+        dataMap.put("avatarLink", getAvatarLink());
+        dataMap.put("phone", getPhoneNumber());
+        dataMap.put("username", getUserName());
+        String[] events = {"0","2"};
+        List attendingEvents = new ArrayList<>(Arrays.asList(events));
+        dataMap.put("attendingEvents", attendingEvents);
+        String[] friendsList = {"sojasd","dsnfa98hsndfkj"};
+        List friends = new ArrayList<>(Arrays.asList(friendsList));
+        dataMap.put("friends", friends);
+        String[] hosting = {};
+        List hostingEvents = new ArrayList<>(Arrays.asList(hosting));
+        dataMap.put("hostingEvents",hostingEvents);
+
+
+        DatabaseReference newEventReference = FirebaseDatabase.getInstance().getReference().child("users")
+                .child(getUID());
+        newEventReference.setValue(dataMap).addOnCompleteListener(onCompleteListener);
     }
 
 }
