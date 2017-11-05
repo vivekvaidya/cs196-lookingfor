45e84ad44edb92ecdbd7f94fb8491ed32eeb637a
Dun Ma <madun@hotmail.com>
2017-11-05 00:46:23 -0500

    smoother scroll of listView

--- LookingFor/app/src/main/java/vivekvaidya/com/lookingfor/EventBrowserItemAdapter.java
+++ LookingFor/app/src/main/java/vivekvaidya/com/lookingfor/EventBrowserItemAdapter.java
@@ -12,12 +12,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -70,35 +72,64 @@ public class EventBrowserItemAdapter extends BaseAdapter implements ListAdapter 
         return i;
     }
 
+    static class ViewHolder {
+        TextView titleLabel;
+        TextView dateTimeLabel;
+        TextView typeLabel;
+        TextView locationLabel;
+        TextView descriptionLabel;
+        TextView tagLabel;
+        ImageView hostAvatar;
+        ImageView attendeeAvatar;
+        Button attendEventButton;
+    }
+
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        /**UI Variables*/
-        View myView = inflater.inflate(R.layout.event_item_layout, viewGroup, false);
-        TextView titleLabel = (TextView) myView.findViewById(R.id.titleLabel);
-        TextView dateTimeLabel = (TextView) myView.findViewById(R.id.dateTimeLabel);
-        TextView typeLabel = (TextView) myView.findViewById(R.id.typeLabel);
-        TextView locationLabel = (TextView) myView.findViewById(R.id.locationLabel);
-        TextView descriptionLabel = (TextView) myView.findViewById(R.id.descriptionLabel);
-        TextView tagLabel = (TextView) myView.findViewById(R.id.tagLabel);
-        final ImageView hostAvatar = (ImageView) myView.findViewById(R.id.hostAvatar);
-        ImageView attendeeAvatar = (ImageView) myView.findViewById(R.id.attendeeImage);
-        Button attendEventButton = (Button) myView.findViewById((R.id.attendEventButton));
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+
+        ViewHolder mViewHolder;
+        if (convertView == null) {
+            /**UI Variables*/
+            mViewHolder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.event_item_layout, viewGroup, false);
+            mViewHolder.titleLabel = (TextView) convertView.findViewById(R.id.titleLabel);
+            mViewHolder.dateTimeLabel = (TextView) convertView.findViewById(R.id.dateTimeLabel);
+            mViewHolder.typeLabel = (TextView) convertView.findViewById(R.id.typeLabel);
+            mViewHolder.locationLabel = (TextView) convertView.findViewById(R.id.locationLabel);
+            mViewHolder.descriptionLabel = (TextView) convertView.findViewById(R.id.descriptionLabel);
+            mViewHolder.tagLabel = (TextView) convertView.findViewById(R.id.tagLabel);
+            mViewHolder.hostAvatar = (ImageView) convertView.findViewById(R.id.hostAvatar);
+            mViewHolder.attendeeAvatar = (ImageView) convertView.findViewById(R.id.attendeeImage);
+            mViewHolder.attendEventButton = (Button) convertView.findViewById((R.id.attendEventButton));
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+
+        }
 
         /**Set UI Displays*/
         Event currentEvent = events.get(i);
-        titleLabel.setText(currentEvent.getTitle());
-        dateTimeLabel.setText(currentEvent.getDateTime());
-        typeLabel.setText(currentEvent.getEventType());
-        locationLabel.setText(currentEvent.getLocation());
-        descriptionLabel.setText(currentEvent.getDescription());
-        tagLabel.setText("Dummy Label");
+        mViewHolder.titleLabel.setText(currentEvent.getTitle());
+        mViewHolder.dateTimeLabel.setText(currentEvent.getDateTime());
+        mViewHolder.typeLabel.setText(currentEvent.getEventType());
+        mViewHolder.locationLabel.setText(currentEvent.getLocation());
+        mViewHolder.descriptionLabel.setText(currentEvent.getDescription());
+        mViewHolder.tagLabel.setText("Dummy Label");
 
         /**Get Avatars*/
 
         String hostID = currentEvent.getHostID();
+
         if (hostID == null) {
-            hostID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            FirebaseUser auth = FirebaseAuth.getInstance().getCurrentUser();
+            if (auth != null){
+                hostID = auth.getUid();
+            } else {
+                Toast.makeText(context,"No current User", Toast.LENGTH_LONG).show();
+                return convertView;
+            }
         }
+
         String attendeeID;
         if (currentEvent.getAttendeeID() == null || currentEvent.getAttendeeID().length == 0) {
             attendeeID = hostID;
@@ -110,11 +141,11 @@ public class EventBrowserItemAdapter extends BaseAdapter implements ListAdapter 
         }
         StorageReference storageReference = FirebaseStorage.getInstance().getReference();
         StorageReference hostAvatarPath = storageReference.child("userAvatar/" + hostID + ".png");
-        Glide.with(this.context).using(new FirebaseImageLoader()).load(hostAvatarPath).into(hostAvatar);
+        Glide.with(this.context).using(new FirebaseImageLoader()).load(hostAvatarPath).into(mViewHolder.hostAvatar);
         StorageReference attendeeAvatarPath = storageReference.child("userAvatar/" + attendeeID + ".png");
-        Glide.with(this.context).using(new FirebaseImageLoader()).load(attendeeAvatarPath).into(attendeeAvatar);
+        Glide.with(this.context).using(new FirebaseImageLoader()).load(attendeeAvatarPath).into(mViewHolder.attendeeAvatar);
 
-        attendEventButton.setOnClickListener(new View.OnClickListener() {
+        mViewHolder.attendEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 /**
@@ -123,7 +154,7 @@ public class EventBrowserItemAdapter extends BaseAdapter implements ListAdapter 
             }
         });
 
-        return myView;
+        return convertView;
     }
 
     @Override
